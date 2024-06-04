d036207de7092a591d65a5e61da01bfc90a5c72a
neel0086 <neelmehta0086@gmail.com>
2024-06-04 12:55:10 +0530

    Basic kafka userapp

---
+++ userapp/userapp/src/main/java/com/userapp/userapp/config/KafkaConfig.java
@@ -0,0 +1,16 @@
+package com.userapp.userapp.config;
+
+import com.userapp.userapp.constants.AppConstants;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.KafkaListener;
+
+@Configuration
+public class KafkaConfig {
+    @KafkaListener(topics = AppConstants.LOCATION_UPDATE_TOPIC, groupId = AppConstants.GROUP_ID)
+    public void updatedLocation(String value) {
+
+
+        System.out.println(value);
+
+    }
+}
---
+++ userapp/userapp/src/main/java/com/userapp/userapp/constants/AppConstants.java
@@ -0,0 +1,6 @@
+package com.userapp.userapp.constants;
+
+public class AppConstants {
+    public static final String LOCATION_UPDATE_TOPIC="location-update";
+    public static final String  GROUP_ID="group-1";
+}
